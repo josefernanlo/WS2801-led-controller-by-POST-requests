85dd02d0107451e4960bc96a70b3e111adba70e8
Jose <josefernanlo2@gmail.com>
2022-12-31 02:33:52 +0100

    singleFrame function

--- controllers/stripController.js
+++ controllers/stripController.js
@@ -1,13 +1,25 @@
 const { getPostData } = require('../utils')
+const leds = require("rpi-ws2801");
+const numberOfLeds = 800;
+const color = require('onecolor');
+
+leds.connect(numberOfLeds);
+writeColor(color('black'));
 
 // @desc    rendes one frame
 // @route   POST /singleFrame
 async function singleFrame(req, res) {
     try {
         const body = await getPostData(req)
-        const { array } = JSON.parse(body)
-        console.log(array)
-      
+        const { arr } = JSON.parse(body)
+        console.log(arr)
+        arr.map(e => {
+            leds.setColor(e.index, [e.r, e.g, e.b])                        // Color of led (Red Green Blue)
+            leds.setChannelPower(e.index, parseInt(e.brightness)/100);     // Brightness of R channel.
+            leds.setChannelPower(e.index + 1, parseInt(e.brightness)/100); // Brightness of G channel.
+            leds.setChannelPower(e.index + 2, parseInt(e.brightness)/100); // Brightness of B channel.
+        });
+        leds.update();
 
         res.writeHead(201, { 'Content-Type': 'application/json' })
         return res.end(JSON.stringify({'render': true}))  
@@ -33,6 +45,13 @@ async function spectacle(req, res) {
     }
 }
 
+function writeColor(newColor) {
+    var r = gamma[Math.trunc(newColor.red()   * 255)];
+    var g = gamma[Math.trunc(newColor.green() * 255)];
+    var b = gamma[Math.trunc(newColor.blue()  * 255)];
+
+    leds.fill(r, g, b);
+}
 
 
 module.exports = {
